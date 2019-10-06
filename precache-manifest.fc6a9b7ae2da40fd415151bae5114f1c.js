b2b723b4275c3306fbf60fbd48b8ebac275ecad9
prof-net <pavel.1c@mail.ru>
2019-10-06 22:16:51 +0300

    Updates

---
+++ precache-manifest.fc6a9b7ae2da40fd415151bae5114f1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fddd680fdf5cfc13f992837d2f73182",
+    "url": "/test-task2/index.html"
+  },
+  {
+    "revision": "a61bdb7434f3bb0e649a",
+    "url": "/test-task2/static/css/main.5357fdf7.chunk.css"
+  },
+  {
+    "revision": "246096249fd9821efba3",
+    "url": "/test-task2/static/js/2.433aed28.chunk.js"
+  },
+  {
+    "revision": "a61bdb7434f3bb0e649a",
+    "url": "/test-task2/static/js/main.65333502.chunk.js"
+  },
+  {
+    "revision": "e4ac10ad1d9f8a3af04c",
+    "url": "/test-task2/static/js/runtime-main.026d47a6.js"
+  }
+]);
